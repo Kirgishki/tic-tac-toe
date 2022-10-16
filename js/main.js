8b5ebd7cd7ec33aa111b8488d75162fd930a29a5
Jovan_Obrencevic <jovan.obrencevic34@gmail.com>
2022-10-16 22:17:05 +0200

    Render know draw values from game array

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 const Gameboard = (() => {
-    const gameboard = ["X","O","X",
-                        "X","O","X",
-                        "X","O","X"];
+    const gameboard = ["","","",
+                        "","","",
+                        "","",""];
     
     const guiGameboard = document.getElementById("gameTable");
     const gameboardFields = Array.from(guiGameboard.children);
 
-
-    gameboardFields.forEach(element => {
-        element.addEventListener('click', function () {
-            let i = this.getAttribute("data-field-id");
-            this.textContent = gameboard[i];
+    const render = function() {
+        gameboardFields.forEach(element => {
+            let i = element.getAttribute("data-field-id");
+            element.textContent = gameboard[i];
         });
-    });
+    }
+
 
 })();
 
